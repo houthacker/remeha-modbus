db7a6d24934b9ebcc89fe63e44b1b093b44e110b
houthacker <houthacker@pm.me>
2026-05-25 14:02:35 +0000

    Create fixable issue is switch.heatpump_managed_schedules turned off.

--- custom_components/remeha_modbus/switch.py
+++ custom_components/remeha_modbus/switch.py
@@ -1,13 +1,14 @@
 """Platform for switches in the Remeha Modbus integration."""
 
 import logging
-from typing import cast
+from typing import Any, cast
 
-from homeassistant.components.climate.const import DOMAIN as ClimateDomain
 from homeassistant.components.climate.const import (
+    ATTR_PRESET_MODE,
     PRESET_ECO,
     SERVICE_SET_PRESET_MODE,
 )
+from homeassistant.components.climate.const import DOMAIN as ClimateDomain
 from homeassistant.components.switch import SwitchDeviceClass, SwitchEntity
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import STATE_ON
@@ -112,20 +113,43 @@ class RemehaHeatpumpManagedSchedulesSwitch(RemehaModbusSwitch):
     alrogithms, for example pre-heating an hour before the setpoint must be reached.
     Using the default state is the recommended setting.
 
-    If set to 'off', the affected climate zones are put a mode allowing external
-    preset management. If schedules are synchronized with the `scheduler` component,
+    If set to 'off', the affected climate zones are put in a preset allowing external
+    schedule management. If schedules are synchronized with the `scheduler` component,
     the presets will be set once a schedule timer expires. This effectively disables
     any heat pump heating/cooling algorithms like pre-heating for example. Therefore
     this setting is not recommended.
     """
 
-    async def _async_switch_dhw_climates_to_eco_mode(self) -> None:
+    async def _async_switch_dhw_climates_to_eco_mode(self) -> dict[str, Any]:
+        """Switches all DHW climate presets to ECO.
+
+        This prevents the appliance from running time schedules, allowing time schedule
+        management from Home Assistant directly.
+
+        Returns:
+            A dict of entity ids, mapped to their preset before switching to eco mode.
+            This allows users to apply the issue fix that is proposed when disabling heat pump managed schedules.
+
+        """
+
+        prev_presets: dict[str, str] = {}
         coordinator: RemehaUpdateCoordinator = self._config_entry.runtime_data["coordinator"]
         for climate in coordinator.get_climates(lambda c: c.is_domestic_hot_water()):
             entity_id = get_climate_entity_id(self.hass, climate)
+            state = self.hass.states.get(entity_id)
+
+            # get_climate_entity_id already raises an exception if the entity doesn't exst.
+            if state is None:
+                _LOGGER.warning(
+                    "Cannot switch climate %s to eco mode: its state cannot be retrieved.",
+                    entity_id,
+                )
+                continue
+
+            prev_presets[entity_id] = state.attributes[ATTR_PRESET_MODE]
 
             _LOGGER.debug(
-                "Setting preset_mode of %s to %s to prevent conflicts between heat pump and HA.",
+                "Setting preset_mode of %s to %s to prevent scheduling conflicts between heat pump and Home Assistant.",
                 entity_id,
                 PRESET_ECO,
             )
@@ -135,6 +159,8 @@ class RemehaHeatpumpManagedSchedulesSwitch(RemehaModbusSwitch):
                 service_data={"preset_mode": PRESET_ECO},
                 target={"entity_id": entity_id},
             )
+
+        return prev_presets
 
     async def async_added_to_hass(self):
         """Restore previous state or set to default ('on')."""
@@ -151,14 +177,16 @@ class RemehaHeatpumpManagedSchedulesSwitch(RemehaModbusSwitch):
         await super().async_turn_off()
 
         # Put all DHW climates in eco mode and create an issue.
-        await self._async_switch_dhw_climates_to_eco_mode()
+        undo_presets = await self._async_switch_dhw_climates_to_eco_mode()
 
         ir.async_create_issue(
             hass=self.hass,
             domain=DOMAIN,
+            data={"switch": self.entity_id, **undo_presets},
             issue_id="heatpump_managed_schedules_off",
-            is_fixable=False,
+            is_fixable=True,
             is_persistent=True,
+            issue_domain=DOMAIN,
             learn_more_url=UNEXPECTED_ACTION_ISSUE_URL,
             severity=ir.IssueSeverity.WARNING,
             translation_key="heatpump_managed_schedules_turned_off",
