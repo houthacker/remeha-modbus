79e5a87959347583b7ebee31aadc59a8162aec91
houthacker <houthacker@pm.me>
2026-05-31 15:33:24 +0000

    Add tests for InvalidZoneSchedule repair.

--- tests/test_repairs.py
+++ tests/test_repairs.py
@@ -11,7 +11,10 @@ from homeassistant.helpers import issue_registry as ir
 from custom_components.remeha_modbus.const import (
     DOMAIN,
     ISSUE_DISCOVERY_TABLE_CORRUPTED,
+    ISSUE_INVALID_ZONE_SCHEDULE,
+    REMEHA_ZONE_RESERVED_REGISTERS,
     MetaRegisters,
+    ZoneRegisters,
 )
 from tests.conftest import get_api, setup_platform
 from tests.util.repairs import get_repairs, process_repair_fix_flow, start_repair_fix_flow
@@ -65,3 +68,53 @@ async def test_discovery_table_corrupted_repair(
             MetaRegisters.RESET_DISCOVERY_TABLE.start_address, 1
         )
         assert discovery_register == 0x5A00
+
+
+@pytest.mark.parametrize(
+    "mock_modbus_client", ["modbus_store_invalid_timeslot.json"], indirect=True
+)
+async def test_invalid_zone_schedule(
+    hass: HomeAssistant, mock_modbus_client, mock_config_entry, hass_client, hass_ws_client
+):
+    """Test repairing an invalid zone schedule."""
+
+    api = get_api(mock_modbus_client=mock_modbus_client)
+    with patch(
+        "custom_components.remeha_modbus.api.RemehaApi.create",
+        new=lambda *args, **kwargs: api,
+    ):
+        # Start remeha_modbus
+        await setup_platform(hass=hass, config_entry=mock_config_entry)
+        await hass.async_block_till_done()
+
+        # And modbus register 1201 must contain 0x05a0
+        (timeslot_activity_register,) = await api.async_read_registers(
+            ZoneRegisters.TIME_PROGRAM_MONDAY.start_address + REMEHA_ZONE_RESERVED_REGISTERS, 1
+        )
+        assert timeslot_activity_register == 0xA005
+
+        # Start repair fix flow
+        issue_registry = ir.async_get(hass)
+        issues = await get_repairs(hass, hass_ws_client)
+        assert issues
+        assert issue_registry.async_get_issue(DOMAIN, ISSUE_INVALID_ZONE_SCHEDULE) is not None
+
+        await async_process_repairs_platforms(hass)
+        client = await hass_client()
+        data = await start_repair_fix_flow(client, DOMAIN, ISSUE_INVALID_ZONE_SCHEDULE)
+
+        flow_id = data["flow_id"]
+        assert data["type"] == FlowResultType.FORM
+        assert data["step_id"] == "confirm_overwrite"
+
+        # Issue must have been removed
+        data = await process_repair_fix_flow(client, flow_id, json={})
+        assert data["type"] == FlowResultType.CREATE_ENTRY
+        await hass.async_block_till_done()
+        assert issue_registry.async_get_issue(DOMAIN, ISSUE_INVALID_ZONE_SCHEDULE) is None
+
+        # And modbus register 1201 must contain 0x0100 (was 0x05a0)
+        (timeslot_activity_register,) = await api.async_read_registers(
+            ZoneRegisters.TIME_PROGRAM_MONDAY.start_address + REMEHA_ZONE_RESERVED_REGISTERS, 1
+        )
+        assert timeslot_activity_register == 0x0001
