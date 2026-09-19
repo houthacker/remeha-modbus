44e536f560d76ea837c74fbc34318a07759a5c8c
houthacker <houthacker@pm.me>
2026-09-19 13:49:43 +0000

    Fix blender tests

--- tests/blend/scheduler/test_blender.py
+++ tests/blend/scheduler/test_blender.py
@@ -3,25 +3,23 @@
 from unittest.mock import patch
 
 from homeassistant.core import HomeAssistant
-from pymodbus.client import ModbusBaseClient
 from pytest_homeassistant_custom_component.common import MockConfigEntry
 
 from custom_components.remeha_modbus.blend.blender import BlenderState
 from custom_components.remeha_modbus.blend.scheduler.blender import SchedulerBlender
 from custom_components.remeha_modbus.blend.scheduler.event_dispatcher import EventDispatcher
 from custom_components.remeha_modbus.coordinator import RemehaUpdateCoordinator
-from tests.conftest import remeha_api, setup_platform
+from tests.conftest import setup_platform
 
 
 async def test_blender_creation(
-    hass: HomeAssistant, remeha_modbus_unit: ModbusBaseClient, mock_config_entry: MockConfigEntry
+    hass: HomeAssistant, remeha_api, mock_config_entry: MockConfigEntry
 ):
     """Test that creating a new SchedulerBlender puts it in the expected state."""
 
-    api = remeha_api(remeha_modbus_unit=remeha_modbus_unit)
     with patch(
-        "aio_remeha_modbus.api.api.RemehaApi.create",
-        new=lambda *args, **kwargs: api,
+        "custom_components.remeha_modbus.RemehaApi",
+        new=lambda *args, **kwargs: remeha_api,
     ):
         await setup_platform(hass=hass, config_entry=mock_config_entry)
         await hass.async_block_till_done()
@@ -35,16 +33,15 @@ async def test_blender_creation(
 
 async def test_blender_async_blend(
     hass: HomeAssistant,
-    remeha_modbus_unit: ModbusBaseClient,
+    remeha_api,
     mock_config_entry: MockConfigEntry,
     finalizer: list,
 ):
     """Test that blending a SchedulerBlender transitions it to the STARTED state."""
 
-    api = remeha_api(remeha_modbus_unit=remeha_modbus_unit)
     with patch(
-        "aio_remeha_modbus.api.api.RemehaApi.create",
-        new=lambda *args, **kwargs: api,
+        "custom_components.remeha_modbus.RemehaApi",
+        new=lambda *args, **kwargs: remeha_api,
     ):
         await setup_platform(hass=hass, config_entry=mock_config_entry)
         await hass.async_block_till_done()
